4b72a12266804c2445c3452f034032ec1851af85
cleverdevcharles <cleverdevcharles@gmail.com>
2022-01-21 11:23:27 +0100

    first commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
+  .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
